a12d3b3c4fab65ee887ba94ad644448ebb6b520d
Marc Majcher <majcher@gmail.com>
2018-09-17 16:05:12 -0500

    update

--- js/scooper.js
+++ js/scooper.js
@@ -6,97 +6,113 @@ const multiplier = 1.15;
 const game = new ScooperGame();
 
 function format(num, digits) {
-    const pow = Math.pow(10, digits);
-    let out = (Math.round(num * pow) / pow).toString();
-    const dot = out.match(/\.(\d*)$/);
-    if (dot !== null) {
-        digits -= dot[1].length;
-    }
-    else {
-        out += '.';
-    }
-    return out + '0'.repeat(digits);
+  const pow = Math.pow(10, digits);
+  let out = (Math.round(num * pow) / pow).toString();
+  const dot = out.match(/\.(\d*)$/);
+  if (dot !== null) {
+    digits -= dot[1].length;
+  } else {
+    out += '.';
+  }
+  return out + '0'.repeat(digits);
 }
 
 function el(id) {
-    return document.getElementById(id);
+  return document.getElementById(id);
 }
 
 function setNum(id, value, doFormat = true) {
-    if (doFormat) {
-        value = format(value, 2);
-    }
-    el(id).innerText = value;
+  if (doFormat) {
+    value = format(value, 2);
+  }
+  el(id).innerText = value;
 }
 
 function setClick(id, fn) {
-    el(id).addEventListener('click', fn);
+  el(id).addEventListener('click', fn);
 }
 
 const generators = {
-    scooper: {
-        baseCost: 15,
-        number: 0,
-        rate: 0.1,
-    }
+  scooper: {
+    baseCost: 15,
+    name: 'Scooper',
+    number: 0,
+    rate: 0.1,
+  },
+  super: {
+    baseCost: 100,
+    name: 'Super Scooper',
+    number: 0,
+    rate: 1,
+  }
 };
 
 const player = {
-    money: 0,
+  money: 0,
 };
 
 const flavors = {
-    vanilla: {
-        income: 1
-    }
+  vanilla: {
+    income: 1
+  }
 };
 
 function getCost(type) {
-    return generators[type].baseCost * Math.pow(multiplier, generators[type].number);
+  return generators[type].baseCost * Math.pow(multiplier, generators[type].number);
 }
 
 function buy(type) {
-    const cost = getCost(type);
-    if (cost <= player.money) {
-        generators[type].number++;
-        player.money -= cost;
-    }
+  const cost = getCost(type);
+  if (cost <= player.money) {
+    generators[type].number++;
+    player.money -= cost;
+  }
 }
 
 function scoop(flavor) {
-    player.money += flavors[flavor].income;
+  player.money += flavors[flavor].income;
 }
 
-setClick('scoop-vanilla', (e) => {
-    scoop('vanilla');
+Object.keys(flavors).forEach((flavor) => {
+  setClick(`scoop-${flavor}`, () => {
+    scoop(flavor);
+  });
 });
 
-setClick('buy-scooper', (e) => {
-    buy('scooper');
+Object.keys(generators).forEach((gen) => {
+  setClick(`buy-${gen}`, () => {
+    buy(gen);
+  });
 });
 
 setNum('profit-vanilla', flavors.vanilla.income);
 
 function update(delta) {
-    const scooper = generators.scooper;
-    const ms = delta / 1000;
-    const secIncome = scooper.rate * scooper.number;
-    player.money += (secIncome * ms);
+  const scooper = generators.scooper;
+  const superScooper = generators.super;
+
+  const ms = delta / 1000;
+  let secIncome = 0;
+  secIncome += scooper.rate * scooper.number;
+  secIncome += superScooper.rate * superScooper.number;
+  player.money += (secIncome * ms);
 }
 
-function draw(interpolationPercentage) {
-    setNum('num-money', Math.floor(player.money));
-    setNum('num-scoopers', generators.scooper.number, false);
-    setNum('rate-scooper', generators.scooper.rate);
-    setNum('cost-scooper', getCost('scooper'));
+function draw() {
+  setNum('num-money', Math.floor(player.money));
 
-    el('buy-scooper').disabled = (getCost('scooper') > player.money);
+  Object.keys(generators).forEach((gen) => {
+    setNum(`num-${gen}`, generators[gen].number, false);
+    setNum(`rate-${gen}`, generators[gen].rate);
+    setNum(`cost-${gen}`, getCost(gen));
+    el(`buy-${gen}`).disabled = (getCost(gen) > player.money);
+  });
 }
 
 function end(fps, panic) {
-    if (panic) {
-        console.log(`PANIC: ${fps}`);
-    }
+  if (panic) {
+    console.log(`PANIC: ${fps}`);
+  }
 }
 
 MainLoop.setUpdate(update).setDraw(draw).setEnd(end).start();
