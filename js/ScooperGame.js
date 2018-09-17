33b3a0813124da17e836ab2c14016dd70c69c403
Marc Majcher <majcher@gmail.com>
2018-09-17 16:32:59 -0500

    gameObject

--- js/ScooperGame.js
+++ js/ScooperGame.js
@@ -1,9 +1,124 @@
 /* eslint-env browser */
 
 ((root) => {
+  /* Helper functions */
+
+  function el(id) {
+    return document.getElementById(id);
+  }
+
+  function format(num, digits) {
+    const pow = Math.pow(10, digits); // eslint-disable-line no-magic-numbers
+    let out = (Math.round(num * pow) / pow).toString();
+    const dot = out.match(/\.(\d*)$/);
+    if (dot !== null) {
+      digits -= dot[1].length;
+    } else {
+      out += '.';
+    }
+    return out + '0'.repeat(digits);
+  }
+
+  function setClick(id, fn) {
+    el(id).addEventListener('click', fn);
+  }
+
+  function setNum(id, value, doFormat = true) {
+    if (doFormat) {
+      value = format(value, 2); // eslint-disable-line no-magic-numbers
+    }
+    el(id).innerText = value;
+  }
+
+  /* Main game class, exported */
+
   class ScooperGame {
     constructor() {
-      this.flavor = 'vanilla';
+      this.init();
+    }
+
+    init() {
+      this.multiplier = 1.15;
+
+      this.generators = {
+        scooper: {
+          baseCost: 15,
+          name: 'Scooper',
+          number: 0,
+          rate: 0.1,
+        },
+        super: {
+          baseCost: 100,
+          name: 'Super Scooper',
+          number: 0,
+          rate: 1,
+        }
+      };
+
+      this.player = {
+        money: 0,
+      };
+
+      this.flavors = {
+        vanilla: {
+          income: 1
+        }
+      };
+    }
+
+    setup() {
+      Object.keys(this.flavors).forEach((flavor) => {
+        setClick(`scoop-${flavor}`, () => {
+          this.scoop(flavor);
+        });
+      });
+
+      Object.keys(this.generators).forEach((gen) => {
+        setClick(`buy-${gen}`, () => {
+          this.buy(gen);
+        });
+      });
+
+      setNum('profit-vanilla', this.flavors.vanilla.income);
+    }
+
+    getCost(type) {
+      const gen = this.generators[type];
+      return gen.baseCost * Math.pow(this.multiplier, gen.number);
+    }
+
+    buy(type) {
+      const cost = this.getCost(type);
+      if (cost <= this.player.money) {
+        this.generators[type].number++;
+        this.player.money -= cost;
+      }
+    }
+
+    scoop(flavor) {
+      this.player.money += this.flavors[flavor].income;
+    }
+
+    updateTick(delta) {
+      const scooper = this.generators.scooper;
+      const superScooper = this.generators.super;
+
+      const ms = delta / 1000; // eslint-disable-line no-magic-numbers
+      let secIncome = 0;
+      secIncome += scooper.rate * scooper.number;
+      secIncome += superScooper.rate * superScooper.number;
+      this.player.money += (secIncome * ms);
+    }
+
+    updateView() {
+      setNum('num-money', Math.floor(this.player.money));
+
+      Object.keys(this.generators).forEach((gen) => {
+        setNum(`num-${gen}`, this.generators[gen].number, false);
+        setNum(`rate-${gen}`, this.generators[gen].rate);
+        setNum(`cost-${gen}`, this.getCost(gen));
+        el(`buy-${gen}`).disabled = (this.getCost(gen) > this.player.money);
+      });
     }
   }
 
--- js/scooper.js
+++ js/scooper.js
@@ -2,112 +2,8 @@
 /* eslint-disable no-console, no-magic-numbers */
 /* globals ScooperGame, MainLoop */
 
-const multiplier = 1.15;
 const game = new ScooperGame();
-
-function format(num, digits) {
-  const pow = Math.pow(10, digits);
-  let out = (Math.round(num * pow) / pow).toString();
-  const dot = out.match(/\.(\d*)$/);
-  if (dot !== null) {
-    digits -= dot[1].length;
-  } else {
-    out += '.';
-  }
-  return out + '0'.repeat(digits);
-}
-
-function el(id) {
-  return document.getElementById(id);
-}
-
-function setNum(id, value, doFormat = true) {
-  if (doFormat) {
-    value = format(value, 2);
-  }
-  el(id).innerText = value;
-}
-
-function setClick(id, fn) {
-  el(id).addEventListener('click', fn);
-}
-
-const generators = {
-  scooper: {
-    baseCost: 15,
-    name: 'Scooper',
-    number: 0,
-    rate: 0.1,
-  },
-  super: {
-    baseCost: 100,
-    name: 'Super Scooper',
-    number: 0,
-    rate: 1,
-  }
-};
-
-const player = {
-  money: 0,
-};
-
-const flavors = {
-  vanilla: {
-    income: 1
-  }
-};
-
-function getCost(type) {
-  return generators[type].baseCost * Math.pow(multiplier, generators[type].number);
-}
-
-function buy(type) {
-  const cost = getCost(type);
-  if (cost <= player.money) {
-    generators[type].number++;
-    player.money -= cost;
-  }
-}
-
-function scoop(flavor) {
-  player.money += flavors[flavor].income;
-}
-
-Object.keys(flavors).forEach((flavor) => {
-  setClick(`scoop-${flavor}`, () => {
-    scoop(flavor);
-  });
-});
-
-Object.keys(generators).forEach((gen) => {
-  setClick(`buy-${gen}`, () => {
-    buy(gen);
-  });
-});
-
-setNum('profit-vanilla', flavors.vanilla.income);
-
-function update(delta) {
-  const scooper = generators.scooper;
-  const superScooper = generators.super;
-
-  const ms = delta / 1000;
-  let secIncome = 0;
-  secIncome += scooper.rate * scooper.number;
-  secIncome += superScooper.rate * superScooper.number;
-  player.money += (secIncome * ms);
-}
-
-function draw() {
-  setNum('num-money', Math.floor(player.money));
-
-  Object.keys(generators).forEach((gen) => {
-    setNum(`num-${gen}`, generators[gen].number, false);
-    setNum(`rate-${gen}`, generators[gen].rate);
-    setNum(`cost-${gen}`, getCost(gen));
-    el(`buy-${gen}`).disabled = (getCost(gen) > player.money);
-  });
-}
+game.setup();
 
 function end(fps, panic) {
   if (panic) {
@@ -115,4 +11,8 @@ function end(fps, panic) {
   }
 }
 
-MainLoop.setUpdate(update).setDraw(draw).setEnd(end).start();
+MainLoop.setUpdate((delta) => {
+  game.updateTick(delta);
+}).setDraw(() => {
+  game.updateView();
+}).setEnd(end).start();
